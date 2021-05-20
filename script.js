2a51b11497008a38cdc76e4312a107cca4e3a110
kotak364 <75615832+kotak364@users.noreply.github.com>
2021-05-19 22:28:20 -0800

    update

--- script.js
+++ script.js
@@ -1,40 +1,23 @@
-
-//  Normal js code 
-
 const a = document.querySelector("#btn1");
 const b = document.querySelector("#btn2");
 
+// ON button event
+a.addEventListener("click", () => {
+  const c = "blub" + "2" + ".png";
 
+  document.getElementById("blub1").setAttribute("src", c);
+  document.querySelector("p").textContent = "Blub ON";
+  document.querySelector("p").style.color = "black";
 
-a.addEventListener("click", function () {
-  
-    const c = "blub" + "2" +".png";
+  let e = document.getElementsByTagName("p");
+  console.log(e);
+});
 
-    document.getElementById("blub1").setAttribute('src',c);
-    document.querySelector("p").textContent = "Blub ON";
-    document.querySelector("p").style.color = "black";
+//OFF button event
+b.addEventListener("click", () => {
+  const d = "blub" + "1" + ".png";
 
-
-
- 
-
-})
-
-
-b.addEventListener("click", function () {
-  
-    const d = "blub" + "1" +".png";
-
-    document.getElementById("blub1").setAttribute('src',d);
-    document.querySelector("p").textContent = "Blub OFF";
-    document.querySelector("p").style.color = "blue";
-
-
-
-
- 
-
-})
-
-
-
+  document.getElementById("blub1").setAttribute("src", d);
+   document.querySelector("p").textContent = "Blub OFF";
+  document.querySelector("p").style.color = "blue";
+});
